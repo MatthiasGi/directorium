24ccb9da2d501a996fc83be7d727e9d161e49317
Matthias Gianfelice <matthias@gianfelice.de>
2024-06-16 21:50:23 +0200

    Documentation added

--- src/directorium/utils.py
+++ src/directorium/utils.py
@@ -1,10 +1,32 @@
+"""
+Simple utility module for reused functions that don't fit into any other module.
+"""
+
 from datetime import date, datetime
 
 
 def normalize_date(date: date | datetime) -> date:
+    """Returns a date object from a date or datetime object.
+
+    Args:
+        date: The date or datetime object to normalize.
+
+    Returns:
+        The normalized date object, stripped of time information.
+    """
     return date.date() if isinstance(date, datetime) else date
 
 def easter(year: int | date | datetime) -> date:
+    """Calculates the date of Easter Sunday for a given year.
+
+    If a date or datetime object is passed, the year is extracted from it.
+
+    Args:
+        year: The year for which to calculate Easter Sunday.
+
+    Returns:
+        The date of Easter Sunday for the given year.
+    """
     if isinstance(year, datetime) or isinstance(year, date):
         year = year.year
     a, b, c = year % 19, year // 100, year % 100
